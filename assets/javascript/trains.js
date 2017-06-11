3b424acad5d8b5a036be3b2f84bcbb934ac83ec1
Krista Lane <klane@Kristas-MacBook-Air.local>
2017-06-10 23:20:11 -0700

    updating what I have so far

--- assets/javascript/trains.js
+++ assets/javascript/trains.js
@@ -2,15 +2,15 @@
 
 
   // Initialize Firebase
-  // var config = {
-  //   apiKey: "AIzaSyA2mrxV8z0Qd313Wx5GgZl2c_1pmxZXglw",
-  //   authDomain: "trainscheduler-2170a.firebaseapp.com",
-  //   databaseURL: "https://trainscheduler-2170a.firebaseio.com",
-  //   projectId: "trainscheduler-2170a",
-  //   storageBucket: "trainscheduler-2170a.appspot.com",
-  //   messagingSenderId: "1014874796463"
-  // };
-  // firebase.initializeApp(config);
+  var config = {
+    apiKey: "AIzaSyA2mrxV8z0Qd313Wx5GgZl2c_1pmxZXglw",
+    authDomain: "trainscheduler-2170a.firebaseapp.com",
+    databaseURL: "https://trainscheduler-2170a.firebaseio.com",
+    projectId: "trainscheduler-2170a",
+    storageBucket: "trainscheduler-2170a.appspot.com",
+    messagingSenderId: "1014874796463"
+  };
+  firebase.initializeApp(config);
 
   var dataRef = firebase.database();
 
@@ -18,6 +18,8 @@
   var destination = "";
   var firstTime = "HH:mm";
   var frequency = 0;
+  var nextTime;
+  // var minutesAway = 0;
 
   // Capture Button Click
   $("#add-train").on("click", function(event) {
@@ -29,21 +31,31 @@
       firstTime = $("#time-input").val().trim();
       frequency = $("#frequency-input").val().trim();
 
-  // code to push to firebase
-  dataRef.ref('trainData').push({
-      trainName: trainName,
-      destination: destination,
-      firstTime: firstTime,
-      frequency: frequency,
-      dateAdded: firebase.database.ServerValue.TIMESTAMP
-  });
+  // check if fields are valid inputs.
+  if(trainName != "" && destination !="" && firstTime != "" && frequency != "") {
 
+  if (Number.isInteger(frequency) === false) {
+    alert("Sorry; the frequency field needs to be a number for this to work")
+  };
+
+
+  // dealing with time
+  var now = moment();
+  nextTime = moment(firstTime).toNow();
+  console.log("it happened " + nextTime);
+
+
+  // code to push to firebase
+    dataRef.ref('trainData').push({
+        trainName: trainName,
+        destination: destination,
+        firstTime: firstTime,
+        frequency: frequency,
+        dateAdded: firebase.database.ServerValue.TIMESTAMP
+    });
+  };
   // loader
   dataRef.ref('trainData').on("child_added", function(childSnapshot) {
-
+    $("tbody").append("<tr><td>" + trainName + "<tr><td>" + destination + "<tr><td>" + frequency + "<tr><td>");
   });
-
-    // Output all of the new information into the relevant area
-    $("#train-data > tbody").append("<tr><td>" + trainName + "<tr><td>" + destination + "<tr><td>" + frequency + "<tr><td>")
-
-  });
+});
