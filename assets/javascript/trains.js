b9a6560373e52d2660d3b43dd28a80f327d13acd
Krista Lane <klane@calvisitor-10-105-173-29.calvisitor.1918.berkeley.edu>
2017-06-13 21:08:59 -0700

    removed broken validation code

--- assets/javascript/trains.js
+++ assets/javascript/trains.js
@@ -1,67 +1,62 @@
+$(document).ready(function(){
 
-
-// Initialize Firebase
-var config = {
-  apiKey: "AIzaSyA2mrxV8z0Qd313Wx5GgZl2c_1pmxZXglw",
-  authDomain: "trainscheduler-2170a.firebaseapp.com",
-  databaseURL: "https://trainscheduler-2170a.firebaseio.com",
-  projectId: "trainscheduler-2170a",
-  storageBucket: "trainscheduler-2170a.appspot.com",
-  messagingSenderId: "1014874796463"
-};
-firebase.initializeApp(config);
-
-var dataRef = firebase.database();
-
-
-// Capture Button Click
-$("#add-train").on("click", function(event) {
-  event.preventDefault();
-
-  // Capture User Inputs and store them into variables
-  var trainName = $("#name-input").val().trim();
-  var destination = $("#destination-input").val().trim();
-  var firstTime = $("#time-input").val().trim();
-  var frequency = $("#frequency-input").val().trim();
-
-  // check for valid inputs
-  if (trainName != ""
-    && destination !=""
-    && firstTime != "" && frequency > "00:00" < "25:00"
-    && frequency != "") {
-
-    // push data entered to firebase
-    dataRef.ref('trainData').push({
-        trainName: trainName,
-        destination: destination,
-        firstTime: firstTime,
-        frequency: frequency,
-        dateAdded: firebase.database.ServerValue.TIMESTAMP
-    });
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyA2mrxV8z0Qd313Wx5GgZl2c_1pmxZXglw",
+    authDomain: "trainscheduler-2170a.firebaseapp.com",
+    databaseURL: "https://trainscheduler-2170a.firebaseio.com",
+    projectId: "trainscheduler-2170a",
+    storageBucket: "trainscheduler-2170a.appspot.com",
+    messagingSenderId: "1014874796463"
   };
+  firebase.initializeApp(config);
 
-});
+  var dataRef = firebase.database();
 
-// retrieve and manipulate data from firebase as added.
-dataRef.ref('trainData').on("child_added", function(childSnapshot) {
-  // dealing with time
-  var frequency = childSnapshot.val().frequency;
-  var firstTime = childSnapshot.val().firstTime;
-  var nextTime = moment(firstTime, "hmm");
 
-  // While nextTime is before the current time, add the frequency in minutes until it reaches the current time.
-  while (nextTime.isBefore()) {
-    nextTime.add(frequency, 'minutes');
-  }
-  // calculate the number of minutes the next train is from now.
-  var minutesAway = nextTime.diff(moment(), 'minutes');
+  // Capture Button Click
+  $("#add-train").on("click", function(event) {
+    event.preventDefault();
 
-  // display data in DOM
-  $("tbody").append("<tr>"
-    + "<td>" + childSnapshot.val().trainName + "</td>"
-    + "<td>" + childSnapshot.val().destination + "</td>"
-    + "<td>" + frequency + "</td>"
-    + "<td>" + nextTime.format("hh:mm A") + "</td>"
-    + "<td>" + minutesAway + "</td>"
-    + "</tr>");
+    // Capture User Inputs and store them into variables
+    var trainName = $("#name-input").val().trim();
+    var destination = $("#destination-input").val().trim();
+    var firstTime = $("#time-input").val().trim();
+    var frequency = $("#frequency-input").val().trim();
+    console.log("submitted");
+
+      // push data entered to firebase
+      dataRef.ref('trainData').push({
+          trainName: trainName,
+          destination: destination,
+          firstTime: firstTime,
+          frequency: frequency,
+          dateAdded: firebase.database.ServerValue.TIMESTAMP
+      });
+
+  });
+
+  // retrieve and manipulate data from firebase as added.
+  dataRef.ref('trainData').on("child_added", function(childSnapshot) {
+    // dealing with time
+    var frequency = childSnapshot.val().frequency;
+    var firstTime = childSnapshot.val().firstTime;
+    var nextTime = moment(firstTime, "hmm");
+
+    // While nextTime is before the current time, add the frequency in minutes until it reaches the current time.
+    while (nextTime.isBefore()) {
+      nextTime.add(frequency, 'minutes');
+    }
+    // calculate the number of minutes the next train is from now.
+    var minutesAway = nextTime.diff(moment(), 'minutes');
+
+    // display data in DOM
+    $("tbody").append("<tr>"
+      + "<td>" + childSnapshot.val().trainName + "</td>"
+      + "<td>" + childSnapshot.val().destination + "</td>"
+      + "<td>" + frequency + "</td>"
+      + "<td>" + nextTime.format("hh:mm A") + "</td>"
+      + "<td>" + minutesAway + "</td>"
+      + "</tr>");
+  });
 });
